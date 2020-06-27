25727475cbd18634425ec67b68be01e0444ea76e
theazat <sayanazat@gmail.com>
2020-06-27 12:12:37 +0300

    Lesson 8 (Set up Navigation Component)

--- app/src/main/java/com/azat/runningtracker/ui/MainActivity.kt
+++ app/src/main/java/com/azat/runningtracker/ui/MainActivity.kt
@@ -1,22 +1,32 @@
 package com.azat.runningtracker.ui
 
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.azat.runningtracker.R
-import com.azat.runningtracker.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var runDao: RunDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("runDao", "RUNDAO: ${runDao.hashCode()}")
+
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
     }
 }
--- app/src/main/java/com/azat/runningtracker/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/azat/runningtracker/ui/fragments/SetupFragment.kt
@@ -1,7 +1,11 @@
 package com.azat.runningtracker.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.azat.runningtracker.R
+import kotlinx.android.synthetic.main.fragment_setup.*
 
 /*************************
  * Created by AZAT SAYAN *
@@ -10,4 +14,13 @@ import com.azat.runningtracker.R
  *                       *
  * 22/06/2020 - 10:31 PM  *
  ************************/
-class SetupFragment : Fragment(R.layout.fragment_setup)
+class SetupFragment : Fragment(R.layout.fragment_setup) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+        }
+    }
+}
