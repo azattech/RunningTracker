85e95507d6fa9062cbbfc30b51d309dcb6b08758
theazat <sayanazat@gmail.com>
2020-06-27 18:56:43 +0300

    Lesson 10 (Map's Lifecycle)

--- app/src/main/java/com/azat/runningtracker/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/azat/runningtracker/ui/fragments/TrackingFragment.kt
@@ -1,10 +1,13 @@
 package com.azat.runningtracker.ui.fragments
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.azat.runningtracker.R
 import com.azat.runningtracker.ui.viewmodel.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 /*************************
  * Created by AZAT SAYAN *
@@ -17,4 +20,45 @@ import dagger.hilt.android.AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel: MainViewModel by viewModels()
+
+    private var map: GoogleMap? = null
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync {
+            map = it
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
 }
