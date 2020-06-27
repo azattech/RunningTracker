1be3548a2f096ca3b758969c68b02e0e6d05b32c
theazat <sayanazat@gmail.com>
2020-06-27 13:54:05 +0300

    Lesson 9 (Handling Location Permissions)

---
+++ app/src/main/java/com/azat/runningtracker/other/TrackingUtility.kt
@@ -0,0 +1,33 @@
+package com.azat.runningtracker.other
+
+import android.Manifest
+import android.content.Context
+import android.os.Build
+import pub.devrel.easypermissions.EasyPermissions
+
+
+/*************************
+ * Created by AZAT SAYAN *
+ *                       *
+ * Contact: @theazat     *
+ *                       *
+ * 27/06/2020 - 1:19 PM  *
+ ************************/
+object TrackingUtility {
+
+    fun hasLocationPermission(context: Context) =
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.hasPermissions(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        } else {
+            EasyPermissions.hasPermissions(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+}
