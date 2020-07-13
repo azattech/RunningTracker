0c176ab30bcee7f812c91a355bdb7bf8e76daaba
theazat <sayanazat@gmail.com>
2020-07-13 07:32:43 +0300

    Lesson 16 (Updating the Notification)

--- app/src/main/java/com/azat/runningtracker/services/TrackingService.kt
+++ app/src/main/java/com/azat/runningtracker/services/TrackingService.kt
@@ -18,7 +18,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import com.azat.runningtracker.R
 import com.azat.runningtracker.other.Constants.ACTION_PAUSE_SERVICE
-import com.azat.runningtracker.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.azat.runningtracker.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.azat.runningtracker.other.Constants.ACTION_STOP_SERVICE
 import com.azat.runningtracker.other.Constants.FASTEST_LOCATION_INTERVAL
@@ -28,18 +27,19 @@ import com.azat.runningtracker.other.Constants.NOTIFICATION_CHANNEL_NAME
 import com.azat.runningtracker.other.Constants.NOTIFICATION_ID
 import com.azat.runningtracker.other.Constants.TIMER_UPDATE_INTERVAL
 import com.azat.runningtracker.other.TrackingUtility
-import com.azat.runningtracker.ui.MainActivity
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.maps.model.LatLng
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import javax.inject.Inject
 
 /*************************
  * Created by AZAT SAYAN *
@@ -70,13 +70,20 @@ import timber.log.Timber
 * bind to it so the client would be our fragment in this case but
 * that is much more work and Singleton pattern makes this really much easier.
 * */
+@AndroidEntryPoint
 class TrackingService : LifecycleService() {
 
     var isFirstRun = true
 
+    @Inject
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     private val timeRunInSeconds = MutableLiveData<Long>()
+
+    @Inject
+    lateinit var baseNotificationBuilder: NotificationCompat.Builder
+
+    lateinit var curNotificationBuilder: NotificationCompat.Builder
 
     companion object {
         val timeRunInMillis = MutableLiveData<Long>()
@@ -95,11 +102,13 @@ class TrackingService : LifecycleService() {
 
     override fun onCreate() {
         super.onCreate()
+        curNotificationBuilder = baseNotificationBuilder
         postInitialValues()
         fusedLocationProviderClient = FusedLocationProviderClient(this)
 
         isTracking.observe(this, Observer {
             updateLocationTracking(it)
+            updateNotificationTrackingState(it)
         })
     }
 
@@ -158,6 +167,32 @@ class TrackingService : LifecycleService() {
     private fun pauseService() {
         isTracking.postValue(false)
         isTimerEnabled = false
+    }
+
+    private fun updateNotificationTrackingState(isTracking: Boolean) {
+        val notificationActionText = if (isTracking) "Pause" else "Resume"
+        val pendingIntent = if (isTracking) {
+            val pauseIntent = Intent(this, TrackingService::class.java).apply {
+                action = ACTION_PAUSE_SERVICE
+            }
+            PendingIntent.getService(this, 1, pauseIntent, FLAG_UPDATE_CURRENT)
+        } else {
+            val resumeIntent = Intent(this, TrackingService::class.java).apply {
+                action = ACTION_START_OR_RESUME_SERVICE
+            }
+            PendingIntent.getService(this, 2, resumeIntent, FLAG_UPDATE_CURRENT)
+        }
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        curNotificationBuilder.javaClass.getDeclaredField("mActions").apply {
+            isAccessible = true
+            set(curNotificationBuilder, ArrayList<NotificationCompat.Action>())
+        }
+        curNotificationBuilder = baseNotificationBuilder
+            .addAction(R.drawable.ic_pause_black_24dp, notificationActionText, pendingIntent)
+        notificationManager.notify(NOTIFICATION_ID, curNotificationBuilder.build())
     }
 
     @SuppressLint("MissingPermission")
@@ -225,22 +260,32 @@ class TrackingService : LifecycleService() {
             createNotificationChannel(notificationManager)
         }
 
-        // setAutoCancel(false) will just prevent that if the user clicks on our notification
-        // that the notification disappears we always want to the notification to be active and that's why we have to set
-        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
-            .setAutoCancel(false)
-            .setOngoing(true)
-            .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
-            .setContentTitle("Running App")
-            .setContentText("00:00:00")
-            .setContentIntent(getMainActivityPendingIntent())
+
+        // No need anymore check ServiceModule
+        /*   // setAutoCancel(false) will just prevent that if the user clicks on our notification
+           // that the notification disappears we always want to the notification to be active and that's why we have to set
+           val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+               .setAutoCancel(false)
+               .setOngoing(true)
+               .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
+               .setContentTitle("Running App")
+               .setContentText("00:00:00")
+               .setContentIntent(getMainActivityPendingIntent())*/
 
 
-        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+        startForeground(NOTIFICATION_ID, baseNotificationBuilder.build())
+
+        timeRunInSeconds.observe(this, Observer {
+            val notification = curNotificationBuilder
+                .setContentText(TrackingUtility.getFormattedStopWatchTime(it * 1000L))
+            notificationManager.notify(NOTIFICATION_ID, notification.build())
+        })
     }
 
-    /* Also we need to pass to that notificationBuilder is a PendingIntent. A PendingIntent is used
-    * here to open our MainActivity when we click on our notification */
+    // No need anymore check ServiceModule
+/* Also we need to pass to that notificationBuilder is a PendingIntent. A PendingIntent is used
+    * here to open our MainActivity when we click on our notification *//*
+
     private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
         this,
         0,
@@ -249,6 +294,7 @@ class TrackingService : LifecycleService() {
         },
         FLAG_UPDATE_CURRENT // whenever we launch that PendingIntent and it already exists it will update it instead of recreating or restarting it
     )
+*/
 
 
     /* We need to create a notification with which we will launch this foreground service
