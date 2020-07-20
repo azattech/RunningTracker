f1183bd3e8ea138126e033ff27a81712cf432ffa
theazat <sayanazat@gmail.com>
2020-07-20 08:54:41 +0300

    Lesson 20 (Sorting Runs)

---
+++ app/src/main/java/com/azat/runningtracker/other/SortType.kt
@@ -0,0 +1,12 @@
+package com.azat.runningtracker.other
+
+/*************************
+ * Created by AZAT SAYAN *
+ *                       *
+ * Contact: @theazat     *
+ *                       *
+ * 20/07/2020 - 8:26 AM  *
+ ************************/
+enum class SortType {
+    DATE, RUNNING_TIME, AVG_SPEED, DISTANCE, CALORIES_BURNED
+}
