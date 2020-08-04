763609254c5cd9650ec661536bc1622db73e6d7c
theazat <sayanazat@gmail.com>
2020-08-04 22:56:41 +0300

    Lesson 21 (Saving Username and Weight )

--- app/src/main/java/com/azat/runningtracker/di/AppModule.kt
+++ app/src/main/java/com/azat/runningtracker/di/AppModule.kt
@@ -1,9 +1,15 @@
 package com.azat.runningtracker.di
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import androidx.room.Room
 import com.azat.runningtracker.db.RunningDatabase
+import com.azat.runningtracker.other.Constants.KEY_FIRST_TIME_TOGGLE
+import com.azat.runningtracker.other.Constants.KEY_NAME
+import com.azat.runningtracker.other.Constants.KEY_WEIGHT
 import com.azat.runningtracker.other.Constants.RUNNING_DATABASE_NAME
+import com.azat.runningtracker.other.Constants.SHARED_PREFERENCES_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -45,4 +51,23 @@ object AppModule {
     @Singleton
     @Provides
     fun provideRunDao(db: RunningDatabase) = db.getRunDao()
+
+    @Singleton
+    @Provides
+    fun provideSharedPreferences(@ApplicationContext app: Context) =
+        app.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
+
+    @Singleton
+    @Provides
+    fun provideName(sharedPref: SharedPreferences) = sharedPref.getString(KEY_NAME, "") ?: ""
+
+    @Singleton
+    @Provides
+    fun provideWeight(sharedPref: SharedPreferences) = sharedPref.getFloat(KEY_WEIGHT, 80f)
+
+    @Singleton
+    @Provides
+    fun provideFirsTimeToggle(sharedPref: SharedPreferences) =
+        sharedPref.getBoolean(KEY_FIRST_TIME_TOGGLE, true)
+
 }
