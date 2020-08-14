5f5ceb429060c2b3fc6f943d8fc7269fd2c444bf
theazat <sayanazat@gmail.com>
2020-08-14 07:50:49 +0300

    (Added icon)

--- app/src/main/java/com/azat/runningtracker/di/AppModule.kt
+++ app/src/main/java/com/azat/runningtracker/di/AppModule.kt
@@ -59,7 +59,7 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun provideSharedPreferences(app: Application) =
+    fun provideSharedPreferences(app: Application): SharedPreferences =
         app.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
 
     @Singleton
